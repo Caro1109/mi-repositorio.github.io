d2e925ea4da00d2e5af8125c10f52135f11fd629
Carolina <caroacciapaccia@gmail.com>
2023-09-16 10:33:36 -0300

    pruebamail

--- php/contact.php
+++ php/contact.php
@@ -4,7 +4,11 @@ $fecha = $_POST['date'];
 $fecha = $_POST['date'];
 $celular = $_POST['celular'];
 $mail = $_POST['email'];
-$mensaje = $_POST['consulta'];
+$consulta = $_POST['consulta'];
+$acepta = $_POST['checkbox'];
+$submit = $_POST['submit'];
+
+
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,10 +17,10 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "consulta: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'elhorreoapart@gmail.com';
+$para = 'consultas@elhorreoapart.com.ar';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto,($mensaje), $header);
